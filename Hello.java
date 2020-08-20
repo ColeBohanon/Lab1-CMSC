48f2738069f2deffdd88223cf7ad96cc3e903864
ColeBohanon <colebohanon@gmail.com>
2020-08-20 11:29:16 -0400

    Added Hello.java
    Uploaded Hello.java file for CMSC 255-001

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/****************************
+* Lab 1
+*****************************
+* Cole Bohanon
+* 08/20/20
+* 255-001
+*****************************/
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Cole");
+		System.out.println("Lab 1");
+		System.out.println("Just some beginner code for a lab");
+		System.out.println("08-20-2020");
+		System.out.println("255-001");
+		System.out.println();
+		}
+	}
